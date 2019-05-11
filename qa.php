71b60688b3ba25659af6d9420ccc4bdbb0f90885
hikikomorime <michal@skweres.net>
2019-05-11 13:37:19 +0200

    Update qa.php

--- qa.php
+++ qa.php
@@ -31,24 +31,27 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 */
 
-include_once('gitupdate.php');
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+	include_once 'gitupdate.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
 		$config = array(
-			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-			'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-			'api_url' => 'https://api.github.com/repos/hikikomorime/qa', // the GitHub API url of your GitHub repo
-			'raw_url' => 'https://raw.github.com/hikikomorime/qa/master', // the GitHub raw url of your GitHub repo
-			'github_url' => 'https://github.com/hikikomorime/qa', // the GitHub url of your GitHub repo
-			'zip_url' => 'https://github.com/hikikomorime/qa/zipball/master', // the zip url of the GitHub repo
-			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-			'requires' => '3.0', // which version of WordPress does your plugin require?
-			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-			'readme' => 'README.md', // which file to use as the readme for the version number
-			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'github-updater',
+			'api_url' => 'https://api.github.com/repos/hikikomorime/qa',
+			'raw_url' => 'https://raw.github.com/hikikomorime/qa/master',
+			'github_url' => 'https://github.com/hikikomorime/qa',
+			'zip_url' => 'https://github.com/jhikikomorime/qa/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
 		);
-		new WP_GitHub_Updater($config);
+		new WP_GitHub_Updater( $config );
 	}
-
+}
 
 // Check if we already have Q&A or Q&A Lite installed and running
 if ( !class_exists( 'QA_Core' ) ) {
