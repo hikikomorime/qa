e4cea7f80f836411304ee2ddc49b4d8a9714ec15
hikikomorime <michal@skweres.net>
2019-05-11 13:30:10 +0200

    Update qa.php

--- qa.php
+++ qa.php
@@ -36,10 +36,10 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 		$config = array(
 			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 			'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-			'api_url' => 'https://api.github.com/repos/hikikomorime/cloner', // the GitHub API url of your GitHub repo
-			'raw_url' => 'https://raw.github.com/hikikomorime/cloner/master', // the GitHub raw url of your GitHub repo
-			'github_url' => 'https://github.com/hikikomorime/cloner', // the GitHub url of your GitHub repo
-			'zip_url' => 'https://github.com/hikikomorime/cloner/zipball/master', // the zip url of the GitHub repo
+			'api_url' => 'https://api.github.com/repos/hikikomorime/qa', // the GitHub API url of your GitHub repo
+			'raw_url' => 'https://raw.github.com/hikikomorime/qa/master', // the GitHub raw url of your GitHub repo
+			'github_url' => 'https://github.com/hikikomorime/qa', // the GitHub url of your GitHub repo
+			'zip_url' => 'https://github.com/hikikomorime/qa/zipball/master', // the zip url of the GitHub repo
 			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 			'requires' => '3.0', // which version of WordPress does your plugin require?
 			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
